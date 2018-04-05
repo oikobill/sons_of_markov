f645b6a18dc759047e9c6c422afc4699b923fe05
Vasilis Oikonomou <v.oikonomou@berkeley.edu>
2018-04-04 22:46:09 -0700

    for rico

--- nlp/preprocessing/preprocess_text_file.py
+++ nlp/preprocessing/preprocess_text_file.py
@@ -1,7 +1,33 @@
 import numpy as np
 import nltk
+import re
 
-def read_file(filename):
-	f = open(filename,"r")
-	raw_txt = f.read()
-	return raw_txt
+
+class Parser:
+
+	def __init__(self):
+		self.works = {}
+
+	def read_file(self, filename):
+		f = open(filename,"r")
+		text = f.read()
+		author = find_author(text)
+		title = find_author(text)
+		self.works[author + "/" + title] = text
+
+	def find_author(self, text):	
+		result = re.search('Author:  *.*', text).group(0)
+		return result.strip()[7:].strip()
+
+	def find_title(self, text):
+		result = re.search('Title:  *.*', text).group(0)
+		return result.strip()[7:].strip()
+
+	def print_works(self):
+		print(self.works)
+
+
+parser = Parser()
+parser.read_file("../data/example_project_gutenberg.txt")
+parser.print_works()
+
