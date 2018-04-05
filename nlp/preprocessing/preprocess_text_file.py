78a8a1506e5a630cb757576fb4e8823087b833dd
Vasilis Oikonomou <v.oikonomou@berkeley.edu>
2018-04-04 23:24:36 -0700

    take over nico

--- nlp/preprocessing/preprocess_text_file.py
+++ nlp/preprocessing/preprocess_text_file.py
@@ -1,33 +1,46 @@
 import numpy as np
 import nltk
 import re
+import string
 
 
 class Parser:
 
 	def __init__(self):
-		self.works = {}
+		self.text = None
+		self.author = None
+		self.title = None
 
 	def read_file(self, filename):
 		f = open(filename,"r")
-		text = f.read()
-		author = find_author(text)
-		title = find_author(text)
-		self.works[author + "/" + title] = text
+		self.text = f.read()
+		
 
 	def find_author(self, text):	
 		result = re.search('Author:  *.*', text).group(0)
-		return result.strip()[7:].strip()
+		self.author = result.strip()[7:].strip()
 
 	def find_title(self, text):
 		result = re.search('Title:  *.*', text).group(0)
-		return result.strip()[7:].strip()
+		self.title = result.strip()[7:].strip()
 
-	def print_works(self):
-		print(self.works)
+	def remove_punctuation(self, text):
+		exclude = set(string.punctuation)
+		new_txt = ''.join(ch for ch in s if ch not in exclude)
+
+
+	def to_lowercase(self, text):
+		return text.lower()
+
+	def process(self):
+		txt = self.to_lowercase(self.text)
+		txt = self.remove_punctuation(txt)
+
+
+
+
 
 
 parser = Parser()
-parser.read_file("../data/example_project_gutenberg.txt")
-parser.print_works()
-
+parser.read_file("../data/darwin/2355-0.txt")
+parser.process()
