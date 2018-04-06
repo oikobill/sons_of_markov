c0dc7af8c82a2bda3174767ed85e0910389f9e70
Vasilis Oikonomou <v.oikonomou@berkeley.edu>
2018-04-06 10:41:53 -0700

    fixed some parser bugs, created mcmc functionality, conducted first experiments

--- nlp/preprocessing/parser.py
+++ nlp/preprocessing/parser.py
@@ -1,61 +1,88 @@
 import numpy as np
 import string, nltk, re, glob
+import pandas as pd
 
 
 class Book:
 
-	def set_author(self, _text):
-		try:
-			_author = re.search('Author:  *.*', _text).group(0) #find author line
-			self.author = _author.strip()[7:].strip()
-		except:
-			print('Could not find author \n')
-			self.author = "None"
+    def set_author(self, _text):
+        try:
+            _author = re.search('Author:  *.*', _text).group(0) #find author line
+            self.author = _author.strip()[7:].strip()
+        except:
+            print('Could not find author \n')
+            self.author = "None"
 
-	def set_title(self, _text):
-		try:
-			_title = re.search('Title:  *.*', _text).group(0) #find title line
-			self.title = _title.strip()[7:].strip()
-		except:
-			print('Could not find title \n')
-			self.title = "None"
+    def set_title(self, _text):
+        try:
+            _title = re.search('Title:  *.*', _text).group(0) #find title line
+            self.title = _title.strip()[7:].strip()
+        except:
+            print('Could not find title \n')
+            self.title = "None"
 
-	def set_text(self, _text):
-		i = _text.rfind('***') # find last instance of  ***
-		_text = _text[i:] # erase everything before that
-		allowed = string.ascii_letters + " "
-		new_txt = ''.join(ch for ch in _text if ch in allowed)
-		self.text = new_txt.lower()
+    def set_text(self, _text):
 
-	def create_bigrams(self):
-		self.bigram = list(nltk.bigrams(self.text.split()))
+        # remove some crap from top and bottom
+        signal1 = "*** START OF THIS PROJECT GUTENBERG EBOOK " + self.title.upper() + " ***"
+        signal2 = "*** END OF THIS PROJECT GUTENBERG EBOOK " + self.title.upper() + " ***"
 
-	def print_bigrams(self):
-		print(*map(' '.join, self.bigram), sep=', ')
+        start_index = _text.find(signal1)
+        end_index = _text.find(signal2)
+        _text = _text[start_index+len(signal1):end_index-len(signal2)]
 
-	def __init__(self, _text):
+        # replace new lines, tabs and all other crap with spaces
+        _text = _text.replace("\n", " ").replace("-", " ").replace("--", " ")
+        # removes multiple spaces
+        _text = re.sub(' +',' ',_text)
 
-		self.set_author(_text)
-		self.set_title(_text)
-		self.set_text(_text)
-		self.create_bigrams()
+        # remove all non-letter characters
+        allowed = string.ascii_letters + " "
+        new_txt = ''.join(ch for ch in _text if ch in allowed)
+        self.text = new_txt.lower()
 
-class Library(Book):
+    def list_words(self):
+        words = self.text.split(" ")
+        while "" in words:
+            words.remove("")
+            
+        while " " in words:
+            words.remove(" ")
 
-	def create_Book(self, filename):
-		f = open(filename,"r")
-		text = f.read()
-		new_book = Book(text)
-		self.works[new_book.author + "/" + new_book.title] = new_book
+        self.word_list = words
 
-	def __init__(self):
-		self.works = {}
-		# self.create_Book("../data/test.txt")
-		filename = glob.glob("../data/*") # import all files
-		for file in filename:
-			self.create_Book(file)
+    def count_words(self):
+        self.list_words()
+
+        s = pd.Series(self.word_list)
+        return s.value_counts()
 
 
-library = Library()
-library.works['Nicolas Raga/Test'].print_bigrams()
+    def __init__(self, filename):
+        # read the file
+        f = open(filename,"r")
+        text = f.read()
+        self.set_title(text)
+        self.set_author(text)
+        self.set_text(text)
+
+        
+
+# class Library(Book):
+
+#   def create_Book(self, filename):
+#       f = open(filename,"r")
+#       text = f.read()
+#       new_book = Book(text)
+#       self.works[new_book.author + "/" + new_book.title] = new_book
+
+#   def __init__(self):
+#       self.works = {}
+#       # self.create_Book("../data/test.txt")
+#       filename = glob.glob("../data/*") # import all files
+#       for file in filename:
+#           self.create_Book(file)
+
+
+
 
